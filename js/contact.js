710bcee7c8166ce2e03ccfb75520f4b601b6b9e6
Martina Calluori <martinacalluori@Martinas-MacBook-Pro-2.local>
2017-03-21 23:13:48 -0400

    Final updates

--- js/contact.js
+++
@@ -1,1 +0,0 @@
-the form again."; } else{ $from="From: $name<$email>\r\nReturn-path: $email"; $subject="Message sent using your contact form"; mail("contact@martinacalluori.com", $subject, $message, $from); echo "Email sent!"; } } ?>
